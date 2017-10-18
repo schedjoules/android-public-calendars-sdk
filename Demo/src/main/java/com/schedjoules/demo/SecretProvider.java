d02e5aaf90a7123bf3fdb321d56d12a1e541f758
Gabor Keszthelyi <gabor.keszthelyi@gmail.com>
2017-10-18 17:00:22 +0200

    Update gitignore files to match with ED SDK, add codeStyleSettings.xml. Reformat all source files. #26

--- Demo/src/main/java/com/schedjoules/demo/SecretProvider.java
+++ Demo/src/main/java/com/schedjoules/demo/SecretProvider.java
@@ -1,53 +1,53 @@
 package com.schedjoules.demo;
 
+import android.content.Context;
+
 import org.dmfs.android.calendarcontent.secrets.ISecretProvider;
 import org.dmfs.android.calendarcontent.secrets.Secret;
 import org.dmfs.android.calendarcontent.secrets.SecurityToken;
-
-import android.content.Context;
 
 
 public class SecretProvider implements ISecretProvider
 {
 
-	/**
-	 * The in-app product license key. You get it from your developer console when you open an app and go to "Services & API".
-	 * 
-	 * Make sure you obfuscate this key. It should not appear in plain text in your app package. Consider to use DexGuard.
-	 */
-	private final static String LICENSE_KEY = "Your play store public key";
+    /**
+     * The in-app product license key. You get it from your developer console when you open an app and go to "Services & API".
+     * <p>
+     * Make sure you obfuscate this key. It should not appear in plain text in your app package. Consider to use DexGuard.
+     */
+    private final static String LICENSE_KEY = "Your play store public key";
 
-	/**
-	 * Your SchedJoules API token. Don't use the test key below in production!
-	 * 
-	 * Make sure you obfuscate this key. It should not appear in plain text in your app package. Consider to use DexGuard.
-	 */
-	private final static String API_TOKEN = "0443a55244bb2b6224fd48e0416f0d9c";
-
-
-	@Override
-	public Secret getSecret(Context context, String key, SecurityToken keyFragment)
-	{
-		if (KEY_LICENSE_KEY.equals(key))
-		{
-			return new Secret(context, keyFragment, LICENSE_KEY);
-		}
-		else if (KEY_API_TOKEN.equals(key))
-		{
-			return new Secret(context, keyFragment, API_TOKEN);
-		}
-		throw new IllegalArgumentException("unknown key '" + key + "'");
-	}
+    /**
+     * Your SchedJoules API token. Don't use the test key below in production!
+     * <p>
+     * Make sure you obfuscate this key. It should not appear in plain text in your app package. Consider to use DexGuard.
+     */
+    private final static String API_TOKEN = "0443a55244bb2b6224fd48e0416f0d9c";
 
 
-	@Override
-	public String getSecret(Context context, String key)
-	{
-		if (KEY_LICENSE_KEY.equals(key))
-		{
-			return LICENSE_KEY;
-		}
-		/* don't return API key in this method */
-		throw new IllegalArgumentException("unknown key '" + key + "'");
-	}
+    @Override
+    public Secret getSecret(Context context, String key, SecurityToken keyFragment)
+    {
+        if (KEY_LICENSE_KEY.equals(key))
+        {
+            return new Secret(context, keyFragment, LICENSE_KEY);
+        }
+        else if (KEY_API_TOKEN.equals(key))
+        {
+            return new Secret(context, keyFragment, API_TOKEN);
+        }
+        throw new IllegalArgumentException("unknown key '" + key + "'");
+    }
+
+
+    @Override
+    public String getSecret(Context context, String key)
+    {
+        if (KEY_LICENSE_KEY.equals(key))
+        {
+            return LICENSE_KEY;
+        }
+        /* don't return API key in this method */
+        throw new IllegalArgumentException("unknown key '" + key + "'");
+    }
 }
--- sdk/src/YOURPACKAGE/SecretProvider.java
+++ sdk/src/YOURPACKAGE/SecretProvider.java
@@ -1,60 +1,60 @@
 package YOURPACKAGE.germany;
 
+import android.content.Context;
+
 import org.dmfs.android.calendarcontent.secrets.ISecretProvider;
 import org.dmfs.android.calendarcontent.secrets.Secret;
 import org.dmfs.android.calendarcontent.secrets.SecurityToken;
-
-import android.content.Context;
 
 
 public class SecretProvider implements ISecretProvider
 {
 
-	/**
-	 * Your public key to access Google Play services. You should obfuscate the key, so it won't be included in your APK in plain text. Better yet, use DexGuard.
-	 */
-	private final static String LICENSE_KEY = "your Google play API public key";
+    /**
+     * Your public key to access Google Play services. You should obfuscate the key, so it won't be included in your APK in plain text. Better yet, use
+     * DexGuard.
+     */
+    private final static String LICENSE_KEY = "your Google play API public key";
+
+    /**
+     * Your SchedJoules API token. This value must be obfuscated, so the APK won't contain it in plain text. Consider to use DexGuard.
+     * <p>
+     * Leaked keys will be revoked!
+     */
+    private final static String API_TOKEN = "your SchedJoules API token";
 
 
-	/**
-	 * Your SchedJoules API token. This value must be obfuscated, so the APK won't contain it in plain text. Consider to use DexGuard.
-	 *
-	 * Leaked keys will be revoked!
-	 */
-	private final static String API_TOKEN = "your SchedJoules API token";
+    @Override
+    public Secret getSecret(Context context, String key, SecurityToken token)
+    {
+        if (KEY_LICENSE_KEY.equals(key))
+        {
+            return new Secret(context, token /* just forward the token */, LICENSE_KEY /* pass the license key in plain text here */);
+        }
+        else if (KEY_API_TOKEN.equals(key))
+        {
+            return new Secret(context, token /* just forward the token */, API_TOKEN /* pass the API token in plain text here */);
+        }
+        throw new IllegalArgumentException("unknown key '" + key + "'");
+    }
 
 
-	@Override
-	public Secret getSecret(Context context, String key, SecurityToken token)
-	{
-		if (KEY_LICENSE_KEY.equals(key))
-		{
-			return new Secret(context, token /* just forward the token */, LICENSE_KEY /* pass the license key in plain text here */);
-		}
-		else if (KEY_API_TOKEN.equals(key))
-		{
-			return new Secret(context, token /* just forward the token */, API_TOKEN /* pass the API token in plain text here */);
-		}
-		throw new IllegalArgumentException("unknown key '" + key + "'");
-	}
-
-
-	@Override
-	public String getSecret(Context context, String key)
-	{
-		/*
+    @Override
+    public String getSecret(Context context, String key)
+    {
+        /*
 		 * This method is meant to provide the Google Play License Key to the SDK UI provided by SchedJoules. If you implement your own UI just return null,
 		 * otherwise uncomment the code below to return the key.
 		 *		
 		 * Never ever return the API token here!
 		 */
-		return null;
+        return null;
 
-		// if (KEY_LICENSE_KEY.equals(key))
-		// {
-		// 	return LICENSE_KEY;
-		// }
-		// /* don't return API token by this method !!! */
-		// throw new IllegalArgumentException("unknown key '" + key + "'");
-	}
+        // if (KEY_LICENSE_KEY.equals(key))
+        // {
+        // 	return LICENSE_KEY;
+        // }
+        // /* don't return API token by this method !!! */
+        // throw new IllegalArgumentException("unknown key '" + key + "'");
+    }
 }
